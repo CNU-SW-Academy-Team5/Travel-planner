a44b596a5f1251d694dde4d80d482699711b101e
leesubin <aksgdmsehdan@naver.com>
2023-01-12 20:26:00 +0900

    first upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import React from 'react';
+import Login from './Login';
+import Join from './Join';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Login/>
+    <Join/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Join.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+
+const User ={
+    email: 'sw@naver.com',
+    pw: 'sw12345!!'
+}
+export default function Join(){
+    const [email, setEmail] = useState('');
+    const [pw, setPw] = useState('');
+
+    const [emailValid, setEmailValid] = useState(false);
+    const [pwValid, setPwValid] = useState(false);
+    const [notAllow, setNotAllow] = useState(true);
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+        const regex =
+          /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+        if (regex.test(e.target.value)) {
+          setEmailValid(true);
+        } else {
+          setEmailValid(false);
+        }
+      };
+      const handlePw = (e) => {
+        setPw(e.target.value);
+        const regex =
+          /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+        if (regex.test(e.target.value)) {
+          setPwValid(true);
+        } else {
+          setPwValid(false);
+        }
+      };
+
+    const onClickConfirm = () => {
+        if(email===User.email && pw===User.pw){
+            alert('회원가입 성공');
+        }else{
+            alert('이메일과 비밀번호를 바르게 입력해주세요.');
+        }
+    }
+
+    useEffect(() => {
+    if(emailValid&&pwValid){
+        setNotAllow(false)
+        return;
+    }
+    setNotAllow(true);
+
+    }, [emailValid, pwValid]);
+
+    return(
+        <div className="JoinPage">
+            <div className="JoinTitle">
+                회원가입하기
+            </div>
+
+            <div className="JoinContent">
+            <div className="JoinInputTitle">이메일</div>
+            <div className="JoinInputWrap">
+                <input
+                type='text'
+                className="JoinInput"
+                placeholder="swacademy@naver.com"
+                value={email}
+                onChange={handleEmail}/>
+            </div> 
+            <div className="JoinErrorMessageWrap">
+                {!emailValid&&email.length>0&&(
+                        <div>올바른 이메일을 입력해주세요</div>
+                )}
+            </div>
+            <div style={{marginTop: "6px"}}className="JoinInputTitle">비밀번호</div>
+            <div className="JoinInputWrap">
+                <input
+                type='password'
+                className="JoinInput"
+                placeholder="영문, 숫자, 특수문자 포함 8자 이상 입력해주세요."
+                value={pw}
+                onChange={handlePw}/>
+            </div> 
+            <div className="JoinErrorMessageWrap">
+                {!pwValid && pw.length>0&&(
+                    <div>영문, 숫자, 특수문자 포함 8자 이상</div>
+                )}
+            </div>
+            </div>
+
+            <div>
+                <button onClick={onClickConfirm} disabled={notAllow} className="JoinBottomButton">회원가입</button>
+            </div>
+        </div>
+    );
+}
