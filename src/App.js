1e699c1d213be8daa2c8baf1f9872c91e66f8d63
Kyeongdoo Hur <1kdhur2@gmail.com>
2023-01-29 20:47:15 +0900

    connect backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
-import React from 'react';
+// import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ListPlus from './pages/listplus/ListPlus';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
@@ -11,20 +12,41 @@ import ListShare from './pages/listshare/ListShare';
 import Main from './pages/main/Main';
 
 function App() {
+  const [message, setMessage]=useState([]);
+  useEffect(()=>{
+    fetch("/hello")
+        .then((res)=>{
+          return res.json();
+        })
+        .then((data)=>{
+            setMessage(data);
+        });
+  },[]);
+
   return (
-
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<><Login/> <Join/></>}></Route>
-          <Route path='home' element={<Home/>}></Route>
-          <Route path='listplus' element={<><ListPlus/> <ListShare/></>}></Route>
-          <Route path='main' element={<Main/>}></Route>
-        </Routes>
-      </BrowserRouter>
-
+      <header className="App-header">
+        // 기본코드
+        <ul>
+          {message.map((v,idx)=><li key={`${idx}-${v}`}>{v}</li>)}
+        </ul>
+      </header>
     </div>
   );
+  // return (
+
+  //   <div className="App">
+  //     <BrowserRouter>
+  //       <Routes>
+  //         <Route path='/' element={<><Login/> <Join/></>}></Route>
+  //         <Route path='home' element={<Home/>}></Route>
+  //         <Route path='listplus' element={<><ListPlus/> <ListShare/></>}></Route>
+  //         <Route path='main' element={<Main/>}></Route>
+  //       </Routes>
+  //     </BrowserRouter>
+
+  //   </div>
+  // );
 }
 
 export default App;
