933a4a432f3aa801a75dfcd0579fc060fdd0058b
leesubin <aksgdmsehdan@naver.com>
2023-02-15 11:41:56 +0900

    update main

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -13,7 +13,20 @@ export default function Main(){
   const handleClick = () => {
     setDivsMemo([...divs, <div key={divs.length} className='plusMemo'></div>]);
   };
+
+const [inputs, setInputs] = useState([{ value: "" }]);
   
+  const handleAddInput = () => {
+    const newInput = { value: "" };
+    setInputs([...inputs, newInput]);
+  };
+
+  const handleInputChange = (index, event) => {
+    const newInputs = [...inputs];
+    newInputs[index].value = event.target.value;
+    setInputs(newInputs);
+  };
+ 
  const [helloData, setHelloData] = useState([]);
 
   useEffect(() => {
@@ -34,7 +47,7 @@ export default function Main(){
         <p key={index}>{data}</p>
         ))}*/}
         <div>
-        <button className="plusbtn" onClick={() => setDivs([...divs, <Draggable><div className='pluscircle' key={divs.length}></div></Draggable>])}>
+        <button className="plusBtn" onClick={() => setDivs([...divs, <Draggable><div className='plusCircle' key={divs.length}></div></Draggable>])}>
         일정 추가
         </button>
         {divs.map((div, index) => (
@@ -53,10 +66,50 @@ export default function Main(){
         <Draggable>
           <div className="circle">{date.toLocaleDateString()}</div>
         </Draggable>
+        <div className="memo-container">
         <div className="memo">
-  
+        <div>
+      {inputs.map((input, index) => (
+        <div key={index}>
+          <input className="memoText"
+            type="text"
+            value={input.value}
+            onChange={(event) => handleInputChange(index, event)}
+          />
         </div>
-      </div>
-        
+      ))}
+      <button className="textBtn" onClick={handleAddInput}>장소 +</button>
+    </div>
+        </div>
+        <div className="memo">
+        <div>
+      {inputs.map((input, index) => (
+        <div key={index}>
+          <input className="memoText2"
+            type="text"
+            value={input.value}
+            onChange={(event) => handleInputChange(index, event)}
+          />
+        </div>
+      ))}
+      <button className="textBtn2" onClick={handleAddInput}>식당 +</button>
+    </div>
+        </div>
+        <div className="memo">
+        <div>
+      {inputs.map((input, index) => (
+        <div key={index}>
+          <input className="memoText3"
+            type="text"
+            value={input.value}
+            onChange={(event) => handleInputChange(index, event)}
+          />
+        </div>
+      ))}
+      <button className="textBtn3" onClick={handleAddInput}>숙박 및 기타 +</button>
+    </div>
+        </div>
+        </div>  
+      </div>    
     )
 }
